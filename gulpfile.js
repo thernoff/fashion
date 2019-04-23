7f361c05e4c23f10bbfc9842c9a04e6070d8a0fc
thernoff <thernoff@yandex.ru>
2019-04-23 17:09:37 +0300

    Урок 10: Настройка и подключение плагина Owlcarousel 2 для слайдера

--- gulpfile.js
+++ gulpfile.js
@@ -117,10 +117,10 @@ gulp.task('serve', function (done) {
   // Если наблюдаемые файлы будут меняться, то будем запускать соответствующие таски
   gulp.watch("scss/**/*.scss", gulp.series(["sass"]));
   gulp.watch("*.html", gulp.series(["html"]));
-  gulp.watch("js/**/*/*.js", gulp.series(["js"]));
-  gulp.watch("css/**/*/*.css", gulp.series(["css"]));
-  gulp.watch("img/**/*/*.{png,jpg}", gulp.series(["allimages"]));
-  gulp.watch("img/**/*/*.{svg}", gulp.series(["svg"]));
+  gulp.watch("js/**/*.js", gulp.series(["js"]));
+  gulp.watch("css/**/*.css", gulp.series(["css"]));
+  gulp.watch("img/**/*.{png,jpg}", gulp.series(["allimages"]));
+  gulp.watch("img/**/*.{svg}", gulp.series(["svg"]));
 
   done();
 });
---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $('.slider .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    navText: [],
+    responsive: {
+      0: {
+        items: 1,
+        dots: true,
+        nav: false
+      },
+      480: {
+        items: 1
+      }
+    }
+  })
+});
